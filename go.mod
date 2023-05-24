f7fa23e471a5355f28fee94d82729bd53ad4d9a5
Franco Valencia <franco.avalencia@gmail.com>
2023-05-24 12:55:53 -0600

    MVP with Go + mediamtx

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/vanclief/go-sentinel-backend
 
 go 1.20
+
+require github.com/makiuchi-d/gozxing v0.1.1
+
+require (
+	github.com/3d0c/gmf v0.0.0-20220906170454-be727bc5b56c // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	_ "image/png"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/multi/qrcode"
+
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// scan_image("./tmp/frame_3.png")
+	// watch()
+	ticker := time.NewTicker(500 * time.Millisecond)
+	for range ticker.C {
+		scanDir("./tmp/")
+	}
+}
+
+func scanDir(dir string) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		path := filepath.Join(dir, f.Name())
+		if f.IsDir() || !isImage(path) {
+			continue
+		}
+
+		fmt.Println("Scan:", path)
+		go scan_image(path)
+	}
+}
+
+func watch() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				if event.Op&fsnotify.Create == fsnotify.Create || event.Op&fsnotify.Write == fsnotify.Write {
+					if isImage(event.Name) {
+						fmt.Println("Scan:", event.Name)
+						scan_image(event.Name)
+						os.Remove(event.Name)
+					}
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("./tmp/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
+func isImage(path string) bool {
+	ext := strings.ToLower(filepath.Ext(path))
+	return ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif"
+}
+
+func scan_image(path string) {
+
+	defer os.Remove(path)
+
+	// fmt.Println("path", path)
+
+	file, err := os.Open(path)
+	if err != nil {
+		// fmt.Println("Open err", err)
+		return
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		// fmt.Println("Image err", err)
+		return
+	}
+
+	// prepare BinaryBitmap
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		// fmt.Println("BinaryBitmap err", err)
+		return
+	}
+
+	// decode image
+	qrReader := qrcode.NewQRCodeMultiReader()
+	result, err := qrReader.DecodeMultipleWithoutHint(bmp)
+	if err != nil {
+		// fmt.Println("QRReader Error", err)
+		return
+	}
+
+	fmt.Println("===== QR FOUND =====")
+	fmt.Println(result)
+
+	// hints := make(map[gozxing.DecodeHintType]interface{})
+	// possibleFormats := []gozxing.BarcodeFormat{
+	// 	gozxing.BarcodeFormat_EAN_13,
+	// 	gozxing.BarcodeFormat_EAN_8,
+	// 	gozxing.BarcodeFormat_UPC_A,
+	// 	gozxing.BarcodeFormat_UPC_E,
+	// 	gozxing.BarcodeFormat_CODE_39,
+	// 	gozxing.BarcodeFormat_CODE_93,
+	// 	gozxing.BarcodeFormat_CODE_128,
+	// }
+	//
+	// hints[gozxing.DecodeHintType_POSSIBLE_FORMATS] = possibleFormats
+
+	// onedReader := oned.NewMultiFormatUPCEANReader(hints)
+	// onedReader := oned.NewMultiFormatUPCEANReader(nil)
+	// res2, err := onedReader.DecodeWithoutHints(bmp)
+	// if err != nil {
+	// 	fmt.Println("OnedReader Error", err)
+	// }
+	//
+	// fmt.Println(res2)
+}
