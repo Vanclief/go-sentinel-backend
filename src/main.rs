5efeb47dd121516ab0493775a212676271e3f441
Franco Valencia <franco.avalencia@gmail.com>
2023-05-25 13:17:04 -0600

    Sound & Write to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use csv::Writer;
 use rodio::{source::Source, Decoder, OutputStream};
 use std::fs;
 use std::fs::File;
@@ -12,10 +13,8 @@ fn main() {
     let dir_path = "./tmp";
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open("sounds/beep.mp3").unwrap());
-    let source = Decoder::new(file).unwrap();
-    stream_handle.play_raw(source.convert_samples()).unwrap();
     let (tx, rx) = mpsc::channel();
+    let mut wtr = Writer::from_path("results.csv").unwrap();
 
     println!("Scanning directory: {}", dir_path);
 
@@ -25,6 +24,12 @@ fn main() {
 
         for received in rx.try_iter() {
             println!("Received: {}", received);
+            wtr.write_record(&[received]);
+            wtr.flush().unwrap();
+
+            let file = BufReader::new(File::open("sounds/beep.mp3").unwrap());
+            let source = Decoder::new(file).unwrap();
+            stream_handle.play_raw(source.convert_samples()).unwrap();
         }
     }
 }
@@ -49,9 +54,8 @@ fn scan_directory(dir_path: &str, tx: mpsc::Sender<String>) -> Result<(), std::i
                         let thread_tx = tx.clone();
 
                         let handle = thread::spawn(move || {
-                            detect_barcodes(&file_name);
+                            detect_barcodes(&file_name, thread_tx);
                             fs::remove_file(&file_name).expect("Failed to remove file");
-                            thread_tx.send(file_name).unwrap();
                         });
                         handles.push(handle);
                     }
@@ -68,17 +72,19 @@ fn scan_directory(dir_path: &str, tx: mpsc::Sender<String>) -> Result<(), std::i
     Ok(())
 }
 
-fn detect_barcodes(file_name: &str) {
+fn detect_barcodes(file_name: &str, sender: mpsc::Sender<String>) {
     let detect_results = rxing::helpers::detect_multiple_in_file(file_name);
     match detect_results {
-        Ok(results) => print_results(results),
+        Ok(results) => print_results(results, sender),
         Err(_) => (),
     };
 }
 
-fn print_results(results: Vec<rxing::RXingResult>) {
+fn print_results(results: Vec<rxing::RXingResult>, thread_tx: mpsc::Sender<String>) {
     for result in results {
-        print_result(result);
+        //results.getBarcodeFormat
+        let text = result.getText();
+        thread_tx.send(text.to_string()).unwrap();
     }
 }
 
