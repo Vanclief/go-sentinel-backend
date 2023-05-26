76a36ff5e7b12369499155cb3d42ad19e6448184
Franco Valencia <franco.avalencia@gmail.com>
2023-05-26 12:30:43 -0600

    Implementing a channel in go for results

--- main.go
+++ main.go
@@ -2,23 +2,16 @@ package main
 
 import (
 	"fmt"
-	"image"
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
 	"time"
 
-	"github.com/makiuchi-d/gozxing"
-	"github.com/makiuchi-d/gozxing/oned"
-	"github.com/makiuchi-d/gozxing/qrcode"
-	"github.com/vanclief/ez"
-
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"github.com/vanclief/go-sentinel-backend/scanner"
 
 	"log"
-	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -26,145 +19,36 @@ func main() {
 	f, err := os.Open("sounds/beep2.mp3")
 	if err != nil {
 		log.Fatal(err)
+		panic(err)
 	}
 
 	streamer, format, err := mp3.Decode(f)
 	if err != nil {
 		log.Fatal(err)
+		panic(err)
 	}
 
 	defer streamer.Close()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
 
-	speaker.Play(streamer)
+	s := scanner.New(true, 10)
 
-	scanner := New(true, 10)
-	// scanner.scanImage("./samples/upca-2/1.png")
+	go func() {
+		for res := range s.ResultStream {
+			fmt.Println("Got result:", res)
+			speaker.Play(streamer)
+			// Handle the result as needed
+		}
+	}()
 
 	ticker := time.NewTicker(500 * time.Millisecond)
 	for range ticker.C {
-		scanner.scanDir(scanner, "./tmp/")
+		fmt.Println("Scan dir")
+		s.ScanDir("./tmp/")
 	}
-}
-
-func (s *Scanner) scanDir(scanner *Scanner, dir string) {
-
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-		path := filepath.Join(dir, f.Name())
-		if f.IsDir() || !isImage(path) {
-			continue
-		}
-
-		s.Semaphore <- true
-
-		go func(path string) {
-			defer func() { <-s.Semaphore }()
-			scanner.scanImage(path)
-		}(path)
-	}
-}
-
-func isImage(path string) bool {
-	ext := strings.ToLower(filepath.Ext(path))
-	return ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif"
-}
-
-type Scanner struct {
-	Semaphore    chan bool
-	QRReader     gozxing.Reader
-	OnedReader   gozxing.Reader
-	DeleteOnScan bool
-}
-
-func New(deleteOnScan bool, maxGoroutines int) *Scanner {
-
-	return &Scanner{
-		Semaphore:    make(chan bool, maxGoroutines),
-		QRReader:     qrcode.NewQRCodeReader(),
-		OnedReader:   oned.NewMultiFormatUPCEANReader(nil),
-		DeleteOnScan: deleteOnScan,
-	}
-}
-
-func (s *Scanner) scanImage(path string) error {
-	const op = "scanImage"
-
-	start := time.Now()
-	defer fmt.Println("Scanned:", path, "in", time.Since(start))
-
-	// load image
-	bmp, err := s.load_image(path)
-	if err != nil {
-		return ez.Wrap(op, err)
-	}
-
-	result, err := s.scanQR(bmp)
-	if err != nil {
-		result, err = s.scanBarcode(bmp)
-		if err != nil {
-			return ez.Wrap(op, err)
-		}
-	}
-
-	fmt.Println("===== QR FOUND =====")
-	fmt.Println(result)
-
-	return nil
-}
-
-func (s *Scanner) scanBarcode(bmp *gozxing.BinaryBitmap) (*gozxing.Result, error) {
-	const op = "scanBarcode"
-
-	result, err := s.OnedReader.DecodeWithoutHints(bmp)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	return result, nil
-}
-
-func (s *Scanner) scanQR(bmp *gozxing.BinaryBitmap) (*gozxing.Result, error) {
-	const op = "scanQR"
-
-	// decode image
-	result, err := s.QRReader.DecodeWithoutHints(bmp)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	return result, nil
-}
-
-func (s *Scanner) load_image(path string) (*gozxing.BinaryBitmap, error) {
-	const op = "load_image"
-
-	if s.DeleteOnScan {
-		defer os.Remove(path)
-	}
-
-	// fmt.Println("path", path)
-
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	// prepare BinaryBitmap
-	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
-	if err != nil {
-		return nil, ez.Wrap(op, err)
-	}
-
-	return bmp, nil
 }
---
+++ scanner/scanner.go
@@ -0,0 +1,141 @@
+package scanner
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/oned"
+	"github.com/makiuchi-d/gozxing/qrcode"
+	"github.com/vanclief/ez"
+)
+
+type Scanner struct {
+	QRReader     gozxing.Reader
+	OnedReader   gozxing.Reader
+	DeleteOnScan bool
+	Semaphore    chan bool
+	ResultStream chan *gozxing.Result
+}
+
+func New(deleteOnScan bool, maxGoroutines int) *Scanner {
+
+	return &Scanner{
+		Semaphore:    make(chan bool, maxGoroutines),
+		ResultStream: make(chan *gozxing.Result),
+		QRReader:     qrcode.NewQRCodeReader(),
+		OnedReader:   oned.NewMultiFormatUPCEANReader(nil),
+		DeleteOnScan: deleteOnScan,
+	}
+}
+
+func (s *Scanner) ScanDir(dir string) error {
+	const op = "Scanner.ScanDir"
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return ez.Wrap(op, err)
+	}
+
+	for _, f := range files {
+		path := filepath.Join(dir, f.Name())
+		if f.IsDir() || !isImage(path) {
+			continue
+		}
+
+		s.Semaphore <- true
+
+		go func(path string) {
+			defer func() { <-s.Semaphore }()
+			res, err := s.ScanImage(path)
+			if err == nil && res != nil {
+				s.ResultStream <- res
+			}
+		}(path)
+	}
+
+	return nil
+}
+
+func isImage(path string) bool {
+	ext := strings.ToLower(filepath.Ext(path))
+	return ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif"
+}
+
+func (s *Scanner) ScanImage(path string) (*gozxing.Result, error) {
+	const op = "scanImage"
+
+	start := time.Now()
+	defer fmt.Println("Scanned:", path, "in", time.Since(start))
+
+	// load image
+	bmp, err := s.load_image(path)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	result, err := s.scanQR(bmp)
+	if err != nil {
+		result, err = s.scanBarcode(bmp)
+		if err != nil {
+			return nil, ez.Wrap(op, err)
+		}
+	}
+
+	return result, nil
+}
+
+func (s *Scanner) scanBarcode(bmp *gozxing.BinaryBitmap) (*gozxing.Result, error) {
+	const op = "scanBarcode"
+
+	result, err := s.OnedReader.DecodeWithoutHints(bmp)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	return result, nil
+}
+
+func (s *Scanner) scanQR(bmp *gozxing.BinaryBitmap) (*gozxing.Result, error) {
+	const op = "scanQR"
+
+	// decode image
+	result, err := s.QRReader.DecodeWithoutHints(bmp)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	return result, nil
+}
+
+func (s *Scanner) load_image(path string) (*gozxing.BinaryBitmap, error) {
+	const op = "load_image"
+
+	if s.DeleteOnScan {
+		defer os.Remove(path)
+	}
+
+	// fmt.Println("path", path)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	// prepare BinaryBitmap
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		return nil, ez.Wrap(op, err)
+	}
+
+	return bmp, nil
+}
