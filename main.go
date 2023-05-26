cadd37c08855146a41b9dd8aed3a10af22621f8a
Franco Valencia <franco.avalencia@gmail.com>
2023-05-26 13:21:40 -0600

    Add IP to get easy URL

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
+	"net"
 	"os"
 	"time"
 
@@ -15,7 +16,10 @@ import (
 
 func main() {
 
-	//
+	fmt.Println("GeoScanner 0.1.0")
+	print_local_ip()
+
+	// Instantiate a new player and scanner
 	sound := player.New()
 	s := scanner.New(true, 10)
 
@@ -52,5 +56,35 @@ func main() {
 	ticker := time.NewTicker(500 * time.Millisecond)
 	for range ticker.C {
 		s.ScanDir("./tmp/")
+	}
+}
+
+func print_local_ip() {
+	// Fetch all network interfaces.
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	for _, i := range interfaces {
+		// Fetch all associated addresses for this interface.
+		addresses, err := i.Addrs()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+
+		// Iterate over all addresses and print the first non-loopback IPv4 address.
+		for _, address := range addresses {
+			// Check if the address is an IP net address type and is not a loopback address.
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				// Check if this is an IPv4 address.
+				if ipnet.IP.To4() != nil {
+					fmt.Printf("Connect to: rtmp://%s:1935/dji\n", ipnet.IP.String())
+					return
+				}
+			}
+		}
 	}
 }
